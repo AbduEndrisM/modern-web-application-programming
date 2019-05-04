0d425b3d81c0fe6b8ce46c701d9fcfe53eaf6cff
AbduEndrisM <beazsasm@gmail.com>
2019-05-04 01:51:19 -0500

    NodeJs - Homework #5 - Exercise 1

---
+++ Homework/LessonFive/excerciseone.js
@@ -0,0 +1,37 @@
+{
+    const axios = require('axios') 
+    const express = require('express');
+    const app = express();
+
+    
+app.enable("case sensetive routing");
+app.enable("trust proxy");
+app.enable("strict routing");
+app.disable("x-powered-by");
+app.disable("etag");
+
+
+    app.get('/users', (request, response) => {
+        response.status(200);
+        response.set('Content-Type', 'text/html');
+        console.log('init 0');
+        (async function () {
+            console.log('init 2')
+            let responseObj = await axios.get('https://randomuser.me/api/?results=10');
+            console.log('init 3');
+
+            responseObj.data.results.forEach(element => {
+                response.write("Name: " + element.name.title +" "+ element.name.first+" "+ element.name.last);
+                response.write('<br>')
+                response.write("Gender: "+element.gender)
+                response.write('<br>')
+                response.write(element.email)
+                response.write('<hr>')
+            });
+            response.end();
+            console.log('init 4');
+        })();
+        console.log('init 5');
+    })
+    app.listen(3000, () => console.log('Running on... 3000'));
+}
