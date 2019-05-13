8645b56fe11ecd8903435bd9f415d2e63e39be09
AbduEndrisM <beazsasm@gmail.com>
2019-05-13 16:53:32 -0500

    intial commit TypeScript

---
+++ Practice/LessonEleven/hellowrold.js
@@ -0,0 +1,9 @@
+var hello = (function () {
+    "Hello Wolrd";
+});
+var Wolrd = function () {
+    "Hello Abdu";
+};
+var abdu = function () {
+    return "Abdu world";
+};
---
+++ Practice/LessonEleven/hellowrold.ts
@@ -0,0 +1,12 @@
+
+var hello = (()=>{
+     "Hello Wolrd";
+});
+
+var Wolrd = ()=> {
+    "Hello Abdu"
+};
+
+var abdu = function(){
+    return "Abdu world"
+}
